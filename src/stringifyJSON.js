f395bed9cd78a56d43396008a04dc6bbb2741b33
mrabuse <cogglemat@gmail.com>
2017-01-16 11:34:35 -0600

    Fix bugs for strings, numbers, booleans, and arrays
    Previously returned "string" for any string rather than an object, did not put the value types in quotes, and joined the array with a space between everything rather than no spaces between brackets and a comma between different items.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,25 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(obj === null) {
+  	return "null";
+  } else if (typeof obj === "string") {
+  	return "\"" + obj + "\"";
+  } else if (typeof obj === "number" || typeof obj === "boolean") {
+  	return obj.toString();
+  } else if (Array.isArray(obj)) {
+  	var arrayString = [];	
+
+  	for(var i = 0; i < obj.length; i++) {
+  		var result = stringifyJSON(obj[i]);
+
+  		arrayString.push(result);
+  	}
+
+  	var arrayStringFinished = arrayString.join(",");
+
+  	return "[" + arrayStringFinished + "]";
+  } 
 };
+
+console.log(stringifyJSON(8));
