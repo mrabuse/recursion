627ae2a1a6b41e837d680608ff2f4ba8a5322388
mrabuse <cogglemat@gmail.com>
2017-01-16 15:09:23 -0600

    Complete getElementsByClassName.js
    Because of the nature of the errors the tests were throwing, I didn't want to push anything until it was obvious everything was working. Discovered while coding that if an element doesn't have a classList, asking element.classList.contains() will not work because you can't run .contains on something that doesn't exist!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var containsClassName = [];
+
+	function innerGetElementsByClassName(firstElement, className) {
+		if(firstElement.classList && firstElement.classList.contains(className)) {
+			containsClassName.push(firstElement);
+		}
+
+		var children = firstElement.childNodes;
+
+		if(firstElement.hasChildNodes) {
+			for(var i = 0; i < children.length; i++) {
+				innerGetElementsByClassName(children[i], className);
+			}
+		}
+	}
+
+	if(this === window) {
+		innerGetElementsByClassName(document.body, className);
+	} else {
+		innerGetElementsByClassName(this, className);
+	};
+		
+	return containsClassName;
 };
